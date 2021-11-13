1fe430037d47e7af2079c35a738e772f3c1a38ff
A1tyn-I <88233890+A1tyn-I@users.noreply.github.com>
2021-11-13 20:50:53 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let message = "Hello";
+
+let name = "Altynai";
+let surname = "Zhumabayeva";
+let date = "19.10.2021";
+let university = "Turan";
+alert(message);
+alert(name);
+alert(surname);
+alert(date);
+alert(university);
